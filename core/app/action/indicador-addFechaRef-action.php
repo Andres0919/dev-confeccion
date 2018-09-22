4b476b3109fc7c18bab913c2b6ca840324922f55
Andres0919 <andres.posada0919@gmail.com>
2018-09-21 23:18:54 -0500

    add muestra fisica

--- core/app/action/indicador-addFechaRef-action.php
+++ core/app/action/indicador-addFechaRef-action.php
@@ -7,6 +7,12 @@
     $fecha->analista_ficha = $_POST['analista_ficha'];
     $fecha->analista_id = Core::$user->id;
     $fecha->addActivityDate();
+    if($fecha->actividad_id == 2 || $fecha->actividad_id == 7){
+        $mf = new IndicadorData();
+        $mf->actReferencia_id = $fecha->getLastId()->id;
+        $mf->isMuestra = $_POST['mf'];
+        $mf->addMuestra();
+    }
 
     $alert = "ACTIVIDAD AGREGADA";
     setcookie("alert", $alert, time()+3);
